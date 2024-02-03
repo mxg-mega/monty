10897bcc4e4dbca449f7b15d900cda8f5addd6f8
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-02-03 17:23:35 +0000

    code refactor

--- pint.c
+++ pint.c
@@ -12,7 +12,8 @@ void pint(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-		return;
+		prep_exit(*stack);
+		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
 }
